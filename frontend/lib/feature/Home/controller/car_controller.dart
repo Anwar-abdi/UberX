d20078f19efd39fa27ffdda3154e58d322e4bcc0
Yahye1142 <yahyemahad658@gmail.com>
2025-02-02 16:31:17 +0300

    car controller

--- frontend/lib/feature/Home/controller/car_controller.dart
+++ frontend/lib/feature/Home/controller/car_controller.dart
@@ -2,12 +2,13 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
-
 import '../../../api_end_points.dart';
 
 class CarController extends GetxController {
   var cars = <dynamic>[].obs;
   var filteredCars = <dynamic>[].obs;
+  var favoritedCarIds = <String>[].obs;
+  var favoritedCars = <dynamic>[].obs;
 
   @override
   void onInit() {
@@ -21,9 +22,14 @@ class CarController extends GetxController {
 
       if (response.statusCode == 200) {
         final List<dynamic> decodedData = json.decode(response.body);
-        cars.assignAll(decodedData);
-        filteredCars.assignAll(decodedData); // Initially, show all cars
-        debugPrint('Successfully loaded cars');
+
+        final List<dynamic> availableCars =
+            decodedData.where((car) => car['available'] == true).toList();
+
+        cars.assignAll(availableCars);
+        filteredCars.assignAll(availableCars);
+        updateFavoritedCars();
+        debugPrint('Successfully loaded available cars');
       } else {
         debugPrint('Failed to load cars: ${response.statusCode}');
       }
@@ -36,7 +42,25 @@ class CarController extends GetxController {
     filteredCars.assignAll(cars.where((car) => car['make'] == brand).toList());
   }
 
+  void favoriteCar(String id) {
+    if (favoritedCarIds.contains(id)) {
+      favoritedCarIds.remove(id);
+    } else {
+      favoritedCarIds.add(id);
+    }
+    updateFavoritedCars();
+  }
+
+  bool isFavorite(String id) {
+    return favoritedCarIds.contains(id);
+  }
+
   void resetFilter() {
     filteredCars.assignAll(cars);
+  }
+
+  void updateFavoritedCars() {
+    favoritedCars.assignAll(
+        cars.where((car) => favoritedCarIds.contains(car['_id'])).toList());
   }
 }
