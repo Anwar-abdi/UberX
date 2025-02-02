c48cdccd98d641573360d741ca6f9758d6f353f3
Yahye1142 <yahyemahad658@gmail.com>
2025-02-02 16:34:56 +0300

    bottom navigation

--- frontend/lib/feature/Home/widget/custom_bottom_navigation_bar.dart
+++ frontend/lib/feature/Home/widget/custom_bottom_navigation_bar.dart
@@ -1,50 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/bottom_nav_controller.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
-  final int currentIndex;
-  final Function(int) onTap;
-
-  const CustomBottomNavigationBar({
-    super.key,
-    required this.currentIndex,
-    required this.onTap,
-  });
+  const CustomBottomNavigationBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        BottomNavigationBar(
-          currentIndex: currentIndex,
-          onTap: onTap,
-          selectedItemColor: Colors.blue, // Blue for selected icon & text
-          unselectedItemColor: Colors.black, // Black for unselected icon & text
-          type: BottomNavigationBarType.fixed, // Ensures all items are visible
+    final BottomNavController controller = Get.find<BottomNavController>();
+
+    return Obx(() => BottomNavigationBar(
+          currentIndex: controller.currentIndex.value,
+          onTap: controller.changeIndex,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.black,
+          type: BottomNavigationBarType.fixed,
           items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
             BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
+                icon: Icon(Icons.explore), label: 'Explore'),
             BottomNavigationBarItem(
-              icon: Icon(Icons.explore),
-              label: 'Explore',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.favorite),
-              label: 'Favorite',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.vpn_key),
-              label: 'Key',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
+                icon: Icon(Icons.favorite), label: 'Favorite'),
+            BottomNavigationBarItem(icon: Icon(Icons.vpn_key), label: 'Key'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
           ],
-        ),
-      ],
-    );
+        ));
   }
 }
